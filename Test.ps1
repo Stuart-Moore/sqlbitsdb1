da6bd70b7e6913be8b4e7f9eaff98d9988cc1b65
stuart moore <stuart@stuart-moore.com>
2022-02-09 20:23:10 +0000

    pathsh

--- Test.ps1
+++ Test.ps1
@@ -6,9 +6,10 @@
     Install-Module dbatools -Force -SkipPublisherCheck
     Import-Module dbatools
 
-    New-Item c:\temp\dbaSecurityScan -ItemType folder
+    New-Item c:\temp\dbaSecurityScan -ItemType Directory
     git clone https://github.com/dataplat/dbasecurityscan.git c:\temp\dbaSecurityScan
     import-module c:\temp\dbaSecurityScan\dbasecurityscan.psd1
 
     $options = New-DbaDacOption -Type Dacpac -Action Publish
+    Set-Location $ENV:GITHUB_WORKSPACE
     Publish-DbaDacPackage -SqlInstance localhost -Database DB1 -DacOption $options -Path .\all1.dacpac
