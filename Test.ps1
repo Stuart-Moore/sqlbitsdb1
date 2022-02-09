750526fd358003553d13a37ca5c36e7a03a9d13c
stuart moore <stuart@stuart-moore.com>
2022-02-09 15:16:24 +0000

    paths

--- Test.ps1
+++ Test.ps1
@@ -1,6 +1,5 @@
     Write-Host "Installing Pester" -ForegroundColor Cyan
     Install-Module Pester -Force -SkipPublisherCheck -MaximumVersion 4.9.0
-    Install-Module Pester -Force -SkipPublisherCheck 
     Write-Host "Installing PSFramework" -ForegroundColor Cyan
     Install-Module PSFramework -Force -SkipPublisherCheck
     Write-Host "Installing dbatools" -ForegroundColor Cyan
@@ -8,7 +7,7 @@
     Import-Module dbatools
 
     git clone https://github.com/dataplat/dbasecurityscan.git ../dbaSecurityScan
-    import-module ../dbaSecurityScan/dbasecurityscan/dbasecurityscan.psd1
+    import-module ../dbaSecurityScan/dbasecurityscan.psd1
 
     $options = New-DbaDacOption -Type Dacpac -Action Publish
     Publish-DbaDacPackage -SqlInstance sql2016 -Database DB1 -DacOption $options -Path c:\temp\db.dacpac
