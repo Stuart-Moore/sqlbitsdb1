52bba78219b93676dd83ec687b3dac00bdc6c4e3
stuart moore <stuart@stuart-moore.com>
2022-02-10 10:16:53 +0000

    more paths 2

--- Test.ps1
+++ Test.ps1
@@ -12,9 +12,11 @@
     git clone https://github.com/dataplat/dbasecurityscan.git .\dbaSecurityScan
 
     Write-Host "Install dss"
-    import-module .\dbaSecurityScan\dbasecurityscan.psd1
+    set-location .\dbaSecurityScan
+    import-module .\dbaSecurityScan.psd1
 
     Write-Host "Install DacPac"
+    Write-Host "Where - $($ENV:GITHUB_WORKSPACE)"
     Set-Location $ENV:GITHUB_WORKSPACE
     $options = New-DbaDacOption -Type Dacpac -Action Publish
     Publish-DbaDacPackage -SqlInstance localhost -Database DB1 -DacOption $options -Path .\all1.dacpac
