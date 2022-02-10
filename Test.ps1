e66dc19b10622ecf7769c8abc4451d94f0e33d4e
stuart moore <stuart@stuart-moore.com>
2022-02-10 08:53:43 +0000

    add messages

--- Test.ps1
+++ Test.ps1
@@ -1,15 +1,20 @@
     Write-Host "Installing Pester" -ForegroundColor Cyan
     Install-Module Pester -Force -SkipPublisherCheck -MaximumVersion 4.9.0
+    Import-Module Pester
     Write-Host "Installing PSFramework" -ForegroundColor Cyan
     Install-Module PSFramework -Force -SkipPublisherCheck
+    Import-Module PSFramework
     Write-Host "Installing dbatools" -ForegroundColor Cyan
     Install-Module dbatools -Force -SkipPublisherCheck
     Import-Module dbatools
 
     New-Item c:\temp\dbaSecurityScan -ItemType Directory
     git clone https://github.com/dataplat/dbasecurityscan.git c:\temp\dbaSecurityScan
+
+    Write-Host "Install dss"
     import-module c:\temp\dbaSecurityScan\dbasecurityscan.psd1
 
-    $options = New-DbaDacOption -Type Dacpac -Action Publish
+    Write-Host "Install DacPac"
     Set-Location $ENV:GITHUB_WORKSPACE
+    $options = New-DbaDacOption -Type Dacpac -Action Publish
     Publish-DbaDacPackage -SqlInstance localhost -Database DB1 -DacOption $options -Path .\all1.dacpac
